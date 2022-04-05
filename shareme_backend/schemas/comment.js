e49aaede884dca05b09fe42da6983b365653123d
itsyeon <itsyeon@umich.edu>
2022-04-05 00:24:29 -0400

    backend

---
+++ shareme_backend/schemas/comment.js
@@ -0,0 +1,22 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "PostedBy",
+      type: "postedBy",
+    },
+    {
+      name: "userId",
+      title: "UserId",
+      type: "string",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
